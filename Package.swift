02014b5b472c8669edd8cab7ee83a57de7be2c59
Joe <joe@iachieved.it>
2016-06-05 17:21:52 -0500

    Correct package reference

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "MQTTClient",
     dependencies:[
-      .Package(url:"/home/josephbell/MQTT", majorVersion:0),
+      .Package(url:"https://github.com/iachievedit/MQTT", majorVersion:0),
       .Package(url:"https://github.com/iachievedit/swiftlog", majorVersion:1)
     ]
 )
